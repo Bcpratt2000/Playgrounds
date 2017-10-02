2f75b327a2bdeb8933205a80dc2fb172fb7b7775
Bcpratt2000 <bcpratt2000@gmail.com>
2017-10-02 12:57:44 -0600

    added sample code to Clases file

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -2,4 +2,31 @@
 
 import UIKit
 
-var str = "Hello, playground"
+public class SimpleClass{
+    
+    private var str: String = ""
+    private var num: Int = 0
+    
+    init(string str: String, number num: Int){
+        self.str=str
+        self.num=num
+    }
+    
+    public func getString() -> String{
+        return str
+    }
+    public func getNumber() -> Int{
+        return num
+    }
+    public func setString(string str: String){
+        self.str = str
+    }
+    public func setNumber(number num: Int){
+        self.num = num
+    }
+    
+    
+}
+
+let simple: SimpleClass = SimpleClass(string: "Hello, World", number: 5)
+print(String(simple.getNumber()) + "\n" + simple.getString())
